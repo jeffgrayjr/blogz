ede3e273ef3260dc8aa22dd831f4fb42727866b3
Jeff Gray <jeffgrayjr@gmail.com>
2018-11-05 18:46:28 -0600

    added datetime to blog post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, request, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,11 +14,13 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     content = db.Column(db.String(1000))
+    date_of_pub = db.Column(db.DateTime)
     
 
-    def __init__(self, title, content):
+    def __init__(self, title, content, date_of_pub):
         self.title = title
         self.content = content
+        self.date_of_pub = date_of_pub
         
 
 
@@ -39,6 +42,7 @@ def add_entry():
     if request.method == 'POST':
         blog_title = request.form['blogtitle']
         blog_content = request.form['blogentry']
+        blog_post_date = datetime.datetime.now()
         
         entry_error = ''
         title_error = ''
@@ -49,11 +53,11 @@ def add_entry():
             entry_error = "Please add some content to your post"
         
         if not title_error and not entry_error:
-            new_entry = Post(blog_title,blog_content)
+            new_entry = Post(blog_title,blog_content, blog_post_date)
             db.session.add(new_entry)
             db.session.commit()
             blog_id =  Post.query.filter_by(title = blog_title).first().id
-            return redirect('./blog?id=' + str(blog_id))
+            return redirect('./blog?id=' + str(blog_id)) 
         else:
             return render_template('add_entry.html', title="Add a post", 
         blog_title=blog_title, title_error=title_error,
